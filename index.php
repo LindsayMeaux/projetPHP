386784f88bf04c480619000b54d1cc59cb44da51
QuentinDll <quentin.delplace13@gmail.com>
2018-09-10 17:28:48 +0200

    essaye de push delplace

--- index.php
+++ index.php
@@ -16,20 +16,22 @@ include 'controller.php';
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="/">Navbar</a>
+            <a id="navbar-brand" class="navbar-brand" href="/">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <?php for ($numberPage = 0; $numberPage < count($allPage); $numberPage++) { ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/page-<?= $numberPage; ?>.html"><?= $xml->page[$numberPage]->menu; ?></a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/page-<?= $numberPage; ?>.html"><?= $xml->page[$numberPage]->menu; ?></a>
+                        </li>
                     <?php } ?>
                 </ul>
             </div>
         </nav>
-        <?= $content; ?>
+        <div class="container">
+            <?= $content; ?>
+        </div>
     </body>
 </html>
