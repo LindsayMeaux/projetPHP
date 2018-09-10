d16ea51fd19708e1ef79e699872bbce9bb1dbc75
lindsaym <lindsay.meaux@gmail.com>
2018-09-10 14:26:17 +0200

    ajout css

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ include 'controller.php';
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="https://fonts.googleapis.com/css?family=Handlee" rel="stylesheet">
+         <link rel="stylesheet" href="assets/css/style.css" />
         <title><?= $title; ?></title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
     </head>
