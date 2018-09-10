2ab53c06f242a12916a1183aac3e41b7c6f626bb
lindsaym <lindsay.meaux@gmail.com>
2018-09-10 17:35:04 +0200

    modification css

--- index.php
+++ index.php
@@ -5,14 +5,13 @@ include 'controller.php';
 <!DOCTYPE html>
 <html>
     <head>
-        <link href="https://fonts.googleapis.com/css?family=Handlee" rel="stylesheet">
-         <link rel="stylesheet" href="assets/css/style.css" />
         <meta charset="utf-8" />
         <title><?= $title; ?></title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+          <link rel="stylesheet" href="assets/css/style.css" />
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
