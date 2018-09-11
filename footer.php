a13e95a4dfb27cd7da2f203e1eff80956d98aa38
Ryan <ryan.elr@outlook.com>
2018-09-11 09:11:50 +0200

    Responsive Yt et Map

--- footer.php
+++ footer.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="assets/css/style.css">
-         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" />
         <title>Footer</title>
     </head>
     <body>
-<footer>
+        <footer>
             <div class="row py-5">
                 <div class="container-fluid">
                     <div class="row">
@@ -30,7 +30,7 @@
                                 Meilleur rapport qualité/prix
 
                         </div>
-                         <div class="col-md-3 col-lg-3 col-xl-3 ">
+                        <div class="col-md-3 col-lg-3 col-xl-3 ">
                             <h4  class="text-uppercase font-weight-bold"><img class ="imgShadow" src="assets/img/Screenshot_2018-09-11 Ocordo Travaux Nantes - Entreprise de rénovation et construction(3).png" /></h4>
                             <p>
                                 Satisfaction clients
