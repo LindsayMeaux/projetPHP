f23d39e568ec87859104c1a30bc1d6353767281e
Ryan <ryan.elr@outlook.com>
2018-09-12 09:11:39 +0200

    Error404Img

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ include 'sendmail.php';
     <head>
         <meta charset="utf-8" />
         <title><?= $title; ?></title>
+        <link href="https://fonts.googleapis.com/css?family=Handlee" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
